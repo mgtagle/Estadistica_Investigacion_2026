470f05a84a2edde969b19d48803597b09f81881d
Marco Posgrado <marco.gonzaleztg@uanl.edu.mx>
2026-08-11 13:53:55 -0600

    crear primer script

---
+++ Scripts/Primer_Script_proyecto.R
@@ -0,0 +1,17 @@
+# Marco Aurelio González Tagle
+# Matrícula
+# 11/08/2026
+
+
+# Ingresar Datos ---------------
+
+altura <- 12
+12 + altura
+altura * altura
+
+# conjunto de datos
+
+dbh <- c(12, 10, 11.5, 13.2, 18.5)
+altura + dbh
+
+# Estimar medias ---------------
